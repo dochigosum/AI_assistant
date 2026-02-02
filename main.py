d234aab55e93f722e478413bfb2f2eba55eba8c7
ZXO1114 <jio45298114@gmail.com>
2026-02-02 23:00:01 +0900

    기본 모델 구성

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+from langchain.chat_models import init_chat_model
+
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+model = init_chat_model("gpt-5-nano")
+
+
